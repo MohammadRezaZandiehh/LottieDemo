1eff1b961746fb2664fb39d92ae56101c5b2e8ba
mohammad <m.zandieh1999@gmail.com>
2021-05-05 15:21:01 +0430

    add all commits

--- app/src/main/java/com/example/lottiedemo/MainActivity.java
+++ app/src/main/java/com/example/lottiedemo/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.lottiedemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.animation.ValueAnimator;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LottieAnimationView lottieAnimationView = findViewById(R.id.animationView_main);
+        Button play_btn = findViewById(R.id.btn_main_play);
+
+        play_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lottieAnimationView.playAnimation();
+            }
+        });
+
+
+        lottieAnimationView.addAnimatorUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                //animation.getAnimatedValue();
+            }
+        });
+
+
+        lottieAnimationView.addAnimatorListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
     }
 }
